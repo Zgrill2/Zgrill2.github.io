f8772ae7518721253a6df37cefd808326c886f07
Dimitri D <yonder-airlift.0y@icloud.com>
2026-01-23 20:14:47 -0500

    remove other stuff

--- char-creator/src/test/setup.ts
+++
@@ -1,4 +0,0 @@
-// Test setup for unit tests - vitest configuration
-
-// Test setup for unit tests
-// This file is loaded before tests run
--- char-creator/src/types/weapon.ts
+++
@@ -1,2 +0,0 @@
-// Re-export weapon types from character for convenience
-export type { Weapon, WeaponType, ShieldType } from './character';
